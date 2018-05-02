6d9a3e8d58f2de3fefb9df89310080c9f6deed4f
zero <1215850394@qq.com>
2018-05-02 23:02:23 +0800

    update doc

--- tests/FoundationTest.php
+++ tests/FoundationTest.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Dancesmile\Foundation;
-use stdclass;
 
 
 require_once  __DIR__."/../vendor/autoload.php";
@@ -9,35 +8,22 @@ require_once  __DIR__."/../vendor/autoload.php";
 /**
  * phpunit test foundation
  */
-class FoundationTest   extends Foundation
+class FoundationTest   extends 
 {
 
 	
     
 }
 
+$obj = new Foundation([
 
-/**
- * 		self::DEBUG     => 'DEBUG',
-        self::INFO      => 'INFO',
-        self::NOTICE    => 'NOTICE',
-        self::WARNING   => 'WARNING',
-        self::ERROR     => 'ERROR',
-        self::CRITICAL  => 'CRITICAL',
-        self::ALERT     => 'ALERT',
-        self::EMERGENCY => 'EMERGENCY',
- * @var FoundationTest
- */
-$obj = new FoundationTest([
-		"param1" => "param1",
-		"debug" =>  true,
-		"log"  => [
-			"name" =>  "application name",
-			"file" =>  "./log.log",
-			"level" => "DEBUG",
-			"permission" => 0777
-		]
-]); 
+	
+  
+
+
+]);
+
+
 
 
 
